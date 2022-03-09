a34738074bb5f6325497c7e3bac85a2091782ce7
gidgidonni <gidgidonni.fierro@ciencias.unam.mx>
2022-03-08 20:15:11 -0600

    translation to english done

--- cifLib/atoms_counter.py
+++ cifLib/atoms_counter.py
@@ -6,17 +6,17 @@ def get_wyckoff_data(structure):
     """
     Return
     ------
-    Regresa una lista de listas que contienen de cada sitio: letra de wyckoff, numero de repeticiones de la letra, 
-    el simbolo del elemento del sitio y la ocupación de ese elemento en el sitio.
+    Returns a list of lists which contains the following for every site: wyckoff letter, number of times a letter repeats,
+    the symbol of the element on the site and the ocupation of that element on the site.
     """
     sitios = structure.sites
-    
+
     symmetry_data = SpacegroupAnalyzer(structure).get_symmetry_dataset()
     wyckoff_letters = symmetry_data["wyckoffs"]
     equiv_atoms = list(symmetry_data["equivalent_atoms"])
-    
+
     conjunto = [[i,j, k.species.elements[0].symbol, k.species.num_atoms] for i, j, k in zip(wyckoff_letters, equiv_atoms, sitios)]
-    
+
     return conjunto
 
 
@@ -24,14 +24,14 @@ def get_multiplicity(conjunto):
     """
     Parameters
     ----------
-    conjunto : lista de listas
-        Cada lista contiene la letra, el numero de wyckoff que otorga pymatgen, la letra del elemento en el sitio
-        y la ocupancia.
-        
+    conjunto : list of lists
+	Every list contains a letter, the wyckoff number assigned by pymatgen, the letter of the element on the site and
+	the occupancy.
+
     Return
     ------
-    Regresa un diccionario. Cada "key" es una tupla de la informacion de la lista del conjunto. el "valor" asociado
-    a cada "key" es la multiplicidad para ese sitio.
+    Returns a dictionary. Every "key" is a tuple of the information contained in the list of the variable "conjunto". The associated
+    "value" for every "key" it's the multiplicity for that site.
     """
     mult_dict = {}
 
@@ -40,34 +40,34 @@ def get_multiplicity(conjunto):
             mult_dict[tuple(e)] = 0
 
         mult_dict[tuple(e)] += 1
-        
+
     return mult_dict
 
 
 def num_atoms(diccionario):
     """
-    Se obtiene de la multiplicación de la multiplicidad por la ocupación del sitio.
+    It is obtained by multiplying the multiplicity times the occupation of the site.
     """
     suma = 0
     for e in diccionario:
         suma += e[-1] * diccionario[e]
-        
+
     return suma
 
 # - - - - - - - - - - Funcion principal - - - - - - - - - -
 def get_num_atoms(file_struct):
     """
-    Funcion que utiliza funciones base para dar el numero de atomos.
-    
+    This function uses other base functions to give the number of atoms.
+
     Return
     ------
     float
-        Numero de atomos de la muestra.
+	Number of atoms of the sample.
     """
     conjunto = get_wyckoff_data(file_struct)
-    
+
     mult_dict = get_multiplicity(conjunto)
-    
+
     atoms = float(num_atoms(mult_dict))
-    
+
     return atoms
