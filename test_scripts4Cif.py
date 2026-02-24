139617723c0864ad3a824fc141bb7492de2c172c
Frank <ffavela@gmail.com>
2026-02-24 12:16:59 -0600

    more tests

--- test_scripts4Cif.py
+++ test_scripts4Cif.py
@@ -8,7 +8,34 @@ def test_countTest():
     expCount = 100
     assert expCount == fnd.getCifCount('dir4Tests/cifs4Tests')
 
+
 def test_sample0():
+    fname = 'dir4Tests/sampleTests/sample0.csv'
+    fmtFile = 'sampleFormats/sample0.fmt'
+    inPath = 'dir4Tests/cifs4Tests'
+    fmtStrList=fmt.getStrFmtList(fmtFile)
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+
+    # Doing all this hassle to avoid race contitions
+    anotherL = sorted([line.split(',') for line in lines],
+                      key = lambda x: x[0])
+    # Yeah very unoptimized but the list is not that long
+    for i, line in enumerate(anotherL):
+         anotherL[i] = [a.strip() for a in line]
+    genCsvL = []
+    for cRoute in fnd.yieldCifRoute(inPath):
+        block = fmt.getBlock(cRoute)
+        evalList=[str(misc.myEval(e, block)) for e in fmtStrList]
+        genCsvL.append(evalList)
+
+    genSorted = sorted(genCsvL, key = lambda x: x[0])
+
+    for aL, gS in zip(anotherL, genSorted):
+        assert aL == gS
+
+
+def test_sample1():
     fname = 'dir4Tests/sampleTests/sample1.csv'
     fmtFile = 'sampleFormats/sample1.fmt'
     inPath = 'dir4Tests/cifs4Tests'
@@ -20,7 +47,86 @@ def test_sample0():
     anotherL = sorted([line.split(',') for line in lines],
                       key = lambda x: x[0])
     # Yeah very unoptimized but the list is not that long
-    anotherL = [a[:-1]+[a[-1].strip()] for a in anotherL]
+    for i, line in enumerate(anotherL):
+         anotherL[i] = [a.strip() for a in line]
+    genCsvL = []
+    for cRoute in fnd.yieldCifRoute(inPath):
+        block = fmt.getBlock(cRoute)
+        evalList=[str(misc.myEval(e, block)) for e in fmtStrList]
+        genCsvL.append(evalList)
+
+    genSorted = sorted(genCsvL, key = lambda x: x[0])
+
+    for aL, gS in zip(anotherL, genSorted):
+        assert aL == gS
+
+
+def test_sample2():
+    fname = 'dir4Tests/sampleTests/sample2.csv'
+    fmtFile = 'sampleFormats/sample2.fmt'
+    inPath = 'dir4Tests/cifs4Tests'
+    fmtStrList=fmt.getStrFmtList(fmtFile)
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+
+    # Doing all this hassle to avoid race contitions
+    anotherL = sorted([line.split(',') for line in lines],
+                      key = lambda x: x[0])
+    # Yeah very unoptimized but the list is not that long
+    for i, line in enumerate(anotherL):
+         anotherL[i] = [a.strip() for a in line]
+    genCsvL = []
+    for cRoute in fnd.yieldCifRoute(inPath):
+        block = fmt.getBlock(cRoute)
+        evalList=[str(misc.myEval(e, block)) for e in fmtStrList]
+        genCsvL.append(evalList)
+
+    genSorted = sorted(genCsvL, key = lambda x: x[0])
+
+    for aL, gS in zip(anotherL, genSorted):
+        assert aL == gS
+
+
+def test_sample3():
+    fname = 'dir4Tests/sampleTests/sample3.csv'
+    fmtFile = 'sampleFormats/sample3.fmt'
+    inPath = 'dir4Tests/cifs4Tests'
+    fmtStrList=fmt.getStrFmtList(fmtFile)
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+
+    # Doing all this hassle to avoid race contitions
+    anotherL = sorted([line.split(',') for line in lines],
+                      key = lambda x: x[0])
+    # Yeah very unoptimized but the list is not that long
+    for i, line in enumerate(anotherL):
+         anotherL[i] = [a.strip() for a in line]
+    genCsvL = []
+    for cRoute in fnd.yieldCifRoute(inPath):
+        block = fmt.getBlock(cRoute)
+        evalList=[str(misc.myEval(e, block)) for e in fmtStrList]
+        genCsvL.append(evalList)
+
+    genSorted = sorted(genCsvL, key = lambda x: x[0])
+
+    for aL, gS in zip(anotherL, genSorted):
+        assert aL == gS
+
+
+def test_sample4():
+    fname = 'dir4Tests/sampleTests/sample4.csv'
+    fmtFile = 'sampleFormats/sample4.fmt'
+    inPath = 'dir4Tests/cifs4Tests'
+    fmtStrList=fmt.getStrFmtList(fmtFile)
+    with open(fname, 'r') as file:
+        lines = file.readlines()
+
+    # Doing all this hassle to avoid race contitions
+    anotherL = sorted([line.split(',') for line in lines],
+                      key = lambda x: x[0])
+    # Yeah very unoptimized but the list is not that long
+    for i, line in enumerate(anotherL):
+         anotherL[i] = [a.strip() for a in line]
     genCsvL = []
     for cRoute in fnd.yieldCifRoute(inPath):
         block = fmt.getBlock(cRoute)
